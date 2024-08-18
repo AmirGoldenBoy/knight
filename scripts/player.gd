1d7df51109a1b1856ce749828c29155b59c6303a
AmirGoldenBoy <fas_eclipto@hotmail.com>
2024-08-17 23:54:15 -0300

    Changes in attack node

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,7 +7,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var health = 100
-
+var damage = 20
 @onready var animated_sprite = $AnimatedSprite2D
 
 
@@ -15,7 +15,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -40,4 +39,8 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
+ 	
+	if Input.is_action_just_pressed("Attack"):
+		animated_sprite.play("Attack")
+	else:
+		animated_sprite.play("idle")
